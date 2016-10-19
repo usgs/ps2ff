bd786b9165e298654b4b7e42ea26f1411a6a3e91
Thompson <emthompson@usgs.gov>
2016-10-19 15:32:45 -0600

    Added more tests

--- tests/RjbRrup_test.py
+++ tests/RjbRrup_test.py
@@ -4,8 +4,8 @@ import shutil
 from impactutils.io.cmd import get_command_output
 
 
-def test_rjb():
-    cmd = "progs/RjbMeanVar.py progs/test_Rjb.ini"
+def test_rjb_WC94():
+    cmd = "progs/RjbMeanVar.py tests/config/test_Rjb_WC94.ini"
     rc, so, se = get_command_output(cmd)
     r1 = pd.DataFrame.from_csv("tests/data/test_Rjb_WC94_mechA_ar1p7_seis0_20_Ratios.csv", header = 6)
     v1 = pd.DataFrame.from_csv("tests/data/test_Rjb_WC94_mechA_ar1p7_seis0_20_Var.csv", header = 6)
@@ -18,13 +18,43 @@ def test_rjb():
     # Clean up
     shutil.rmtree('TestData')
 
-def test_rrup():
-    cmd = "progs/RrupMeanVar.py progs/test_Rrup.ini"
+def test_rjb_S14():
+    cmd = "progs/RjbMeanVar.py tests/config/test_Rjb_S14.ini"
+    rc, so, se = get_command_output(cmd)
+    r1 = pd.DataFrame.from_csv("tests/data/test_Rjb_S14_mechA_ar1p7_seis0_20_Ratios.csv", header = 6)
+    v1 = pd.DataFrame.from_csv("tests/data/test_Rjb_S14_mechA_ar1p7_seis0_20_Var.csv", header = 6)
+    r2 = pd.DataFrame.from_csv("TestData/test_Rjb_S14_mechA_ar1p7_seis0_20_Ratios.csv", header = 6)
+    v2 = pd.DataFrame.from_csv("TestData/test_Rjb_S14_mechA_ar1p7_seis0_20_Var.csv", header = 6)
+
+    pd.util.testing.assert_frame_equal(r1, r2)
+    pd.util.testing.assert_frame_equal(v1, v2)
+
+    # Clean up
+    shutil.rmtree('TestData')
+
+
+def test_rrup_S14():
+    cmd = "progs/RrupMeanVar.py tests/config/test_Rrup_S14.ini"
     rc, so, se = get_command_output(cmd)
     r1 = pd.DataFrame.from_csv("tests/data/test_Rrup_S14_mechA_ar2p0_seis0_15_Ratios.csv", header = 6)
     v1 = pd.DataFrame.from_csv("tests/data/test_Rrup_S14_mechA_ar2p0_seis0_15_Var.csv", header = 6)
     r2 = pd.DataFrame.from_csv("TestData/test_Rrup_S14_mechA_ar2p0_seis0_15_Ratios.csv", header = 6)
     v2 = pd.DataFrame.from_csv("TestData/test_Rrup_S14_mechA_ar2p0_seis0_15_Var.csv", header = 6)
+
+    pd.util.testing.assert_frame_equal(r1, r2)
+    pd.util.testing.assert_frame_equal(v1, v2)
+
+    # Clean up
+    shutil.rmtree('TestData')
+
+
+def test_rrup_WC94():
+    cmd = "progs/RrupMeanVar.py tests/config/test_Rrup_WC94.ini"
+    rc, so, se = get_command_output(cmd)
+    r1 = pd.DataFrame.from_csv("tests/data/test_Rrup_WC94_mechA_ar2p0_seis0_15_Ratios.csv", header = 6)
+    v1 = pd.DataFrame.from_csv("tests/data/test_Rrup_WC94_mechA_ar2p0_seis0_15_Var.csv", header = 6)
+    r2 = pd.DataFrame.from_csv("TestData/test_Rrup_WC94_mechA_ar2p0_seis0_15_Ratios.csv", header = 6)
+    v2 = pd.DataFrame.from_csv("TestData/test_Rrup_WC94_mechA_ar2p0_seis0_15_Var.csv", header = 6)
 
     pd.util.testing.assert_frame_equal(r1, r2)
     pd.util.testing.assert_frame_equal(v1, v2)
