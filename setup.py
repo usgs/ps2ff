36ebe4992b2dc7a5f4e134e034b7de53638bceba
C. Bruce Worden <cbworden@usgs.gov>
2017-09-22 15:58:42 -0600

    Added data and config directories to setup.py.

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(name='ps2ff',
       author_email='emthompson@usgs.gov, cbworden@usgs.gov',
       url='http://github.com/usgs/shakelib',
       packages=['ps2ff'],
-      package_data={'ps2ff': [os.path.join('tables', '*')]},
+      package_data={'ps2ff': [os.path.join('tables', '*'),
+                              os.path.join('config', '*'),
+                              os.path.join('data', '*')]},
       scripts=[os.path.join('bin', 'run_ps2ff'), 
                os.path.join('bin', 'run_ps2ff_single_event'),
                os.path.join('bin', 'Cy14HwMeanVar.py')],
       )
-
-
 
