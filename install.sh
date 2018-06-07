8c987a300d0a456d5083655266f80e3d6b3e79d2
emthompson@usgs.gov <emthompson@usgs.gov>
2018-06-07 16:52:47 -0600

    Update install process

--- install.sh
+++ install.sh
@@ -1,60 +1,119 @@
 #!/bin/bash
 
-VENV=ps2ff
-PYVER=3.5
-
-# Is conda installed?
-conda=$(which conda)
-if [ ! "$conda" ] ; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-        -O miniconda.sh;
-    bash miniconda.sh -f -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
+unamestr=`uname`
+env_file=environment.yml
+if [ "$unamestr" == 'Linux' ]; then
+    prof=~/.bashrc
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    matplotlibdir=~/.config/matplotlib
+elif [ "$unamestr" == 'FreeBSD' ] || [ "$unamestr" == 'Darwin' ]; then
+    prof=~/.bash_profile
+    mini_conda_url=https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    matplotlibdir=~/.matplotlib
+else
+    echo "Unsupported environment. Exiting."
+    exit
 fi
 
-conda update -q -y conda
-conda config --prepend channels conda-forge
+source $prof
 
-DEPARRAY=(numpy=1.11 scipy=0.19.1 pandas=0.20.3 pytest=3.2.0 pytest-cov=2.5.1 configobj=5.0.6)
+echo "Path:"
+echo $PATH
 
-# Is the Travis flag set?
-travis=0
-while getopts t FLAG; do
+VENV=ps2ff
+
+
+# Is the reset flag set?
+reset=0
+while getopts r FLAG; do
   case $FLAG in
-    t)
-      travis=1
+    r)
+        reset=1
+        
       ;;
   esac
 done
 
-# Append additional deps that are not for Travis CI
-if [ $travis == 0 ] ; then
-    DEPARRAY+=(ipython=6.1.0 spyder=3.2.1 jupyter=1.0.0 seaborn=0.8.0 \
-        sphinx=1.6.3)
+
+# create a matplotlibrc file with the non-interactive backend "Agg" in it.
+if [ ! -d "$matplotlibdir" ]; then
+    mkdir -p $matplotlibdir
+fi
+matplotlibrc=$matplotlibdir/matplotlibrc
+if [ ! -e "$matplotlibrc" ]; then
+    echo "backend : Agg" > "$matplotlibrc"
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+elif grep -Fxq "backend : Agg" $matplotlibrc ; then
+    :
+elif [ ! grep -Fxq "backend" $matplotlibrc ]; then
+    echo "backend : Agg" >> $matplotlibrc
+    echo "NOTE: A non-interactive matplotlib backend (Agg) has been set for this user."
+else
+    sed -i '' 's/backend.*/backend : Agg/' $matplotlibrc
+    echo "###############"
+    echo "NOTE: $matplotlibrc has been changed to set 'backend : Agg'"
+    echo "###############"
 fi
 
-# Turn off whatever other virtual environment user might be in
-source deactivate
-    
-# Remove any previous virtual environments called pager
-CWD=`pwd`
-cd $HOME;
-conda remove --name $VENV --all -y
-cd $CWD
-    
-# Create a new virtual environment
-conda create --name $VENV -y python=$PYVER ${DEPARRAY[*]}
+
+# Is conda installed?
+conda --version
+if [ $? -ne 0 ]; then
+    echo "No conda detected, installing miniconda..."
+
+    curl $mini_conda_url -o miniconda.sh;
+    echo "Install directory: $HOME/miniconda"
+
+    bash miniconda.sh -f -b -p $HOME/miniconda
+
+    # Need this to get conda into path
+    . $HOME/miniconda/etc/profile.d/conda.sh
+else
+    echo "conda detected, installing $VENV environment..."
+fi
+
+echo "PATH:"
+echo $PATH
+echo ""
+
+
+# Choose an environment file based on platform
+echo ". $HOME/miniconda/etc/profile.d/conda.sh" >> $prof
+
+# If the user has specified the -r (reset) flag, then create an
+# environment based on only the named dependencies, without
+# any versions of packages specified.
+if [ $reset == 1 ]; then
+    echo "Ignoring platform, letting conda sort out dependencies..."
+    env_file=environment.yml
+fi
+
+# Start in conda base environment
+echo "Activate base virtual environment"
+conda activate base
+
+# Create a conda virtual environment
+echo "Creating the $VENV virtual environment:"
+conda env create -f $env_file --force
+
+# Bail out at this point if the conda create command fails.
+# Clean up zip files we've downloaded
+if [ $? -ne 0 ]; then
+    echo "Failed to create conda environment.  Resolve any conflicts, then try again."
+    exit
+fi
+
 
 # Activate the new environment
 echo "Activating the $VENV virtual environment"
-source activate $VENV
-
-# Install some items separately
-pip install https://github.com/usgs/earthquake-impact-utils/archive/master.zip
+conda activate $VENV
 
 # This package
 echo "Installing ps2ff..."
 pip install -e .
 
+# Install default profile
+#python bin/sm_profile -c default -a
+
 # Tell the user they have to activate this environment
-echo "Type 'source activate ${VENV}' to use this new virtual environment."
+echo "Type 'conda activate $VENV' to use this new virtual environment."
